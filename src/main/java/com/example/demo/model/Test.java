2628e6a6cd11c8b521bf26ed9195fefcd547a6c6
changrong.zeng <changrong.zeng@oyohotels.cn>
2020-03-18 19:01:55 +0800

    摩尔投票法

--- src/main/java/com/example/demo/model/Test.java
+++ src/main/java/com/example/demo/model/Test.java
@@ -1,6 +1,5 @@
 package com.example.demo.model;
 
-import sun.management.counter.Counter;
 
 /**
  * @Author changrong.zeng
@@ -11,30 +10,42 @@ public class Test {
 
     public static void main(String[] args) {
         String s1 = "abc";
-        String s2 = "bca";
+        String s2 = "bwc";
 
-        if (checkPermutation(s1,s2)){
-            System.out.println("true");
+        boolean flag = checkPermutation(s1,s2);
 
-        }else{
-            System.out.println("false");
-        }
+        System.out.println("flag=" + flag);
+
+
+        int a = 'a';
+        System.out.println("a="+a);
     }
 
-    public static boolean checkPermutation(String s1, String s2) {
-        if (s1.length()!=s2.length()) return false;
-        int[] temp=new int[256];
-        for (int i = 0; i <s1.length() ; i++) {
-            char c = s1.charAt(i);
-            temp[c]++;
+
+
+
+        public static boolean checkPermutation(String s1, String s2) {
+
+            int[] temp = new int[256];
+            for(int i = 0; i < s1.length(); i++){
+                char c = s1.charAt(i);
+                temp[c]++;
+            }
+
+            for(int i = 0; i < s2.length(); i++){
+                char c = s2.charAt(i);
+                if(temp[c] == 0){
+                    return false;
+                }else{
+                    temp[c]--;
+                }
+            }
+
+            return true;
+
         }
-        for (int i = 0; i <s2.length() ; i++) {
-            char c=s2.charAt(i);
-            if (temp[c]==0) return false;
-            else temp[c]--;
-        }
-        return true;
-    }
+
+
 
 
 }
